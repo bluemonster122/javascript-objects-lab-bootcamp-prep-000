12775fecbb7122c5021849fad763cd13f3185469
L H Gilfoyle <l.gilfoyle@wlv.ac.uk>
2018-07-06 14:06:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) { return Object.assign({}, object, {[key]:value}) }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) { Object.assign(object, {[key]:value}) }
+function deleteFromObjectByKey(object, key) { var ret = Object.assign({}, object); delete ret[key]; return ret }
